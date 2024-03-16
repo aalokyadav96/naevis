07e39c08204410ef5397be5d5bd92a973e86c51f
Aalok Yadav <35891906+aalokyadav96@users.noreply.github.com>
2024-03-16 17:25:31 +0530

    Added file server

--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"os"
+    "net/http"
+    "log"
+    "fmt"
 	"io"
+	"os"
 	"time"
+	"crypto/rand"
+	rndm "math/rand"
+	"crypto/md5"
 	"path/filepath"
+
+    "github.com/julienschmidt/httprouter"
 )
 
-func main() {
-	http.HandleFunc("/",Index)
 
-	http.ListenAndServe(":4000",nil)
+const MAX_UPLOAD_SIZE = 10 * 1024 * 1024 // 10 mb
+
+//const posterPath = "./posters"
+const videoPath = "./videos"
+const imagePath = "./images"
+
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.POST("/", Index)
+	
+	static := httprouter.New()
+	static.ServeFiles("/image/*filepath", http.Dir(imagePath))
+	static.ServeFiles("/video/*filepath", http.Dir(videoPath))
+//	static.ServeFiles("/poster/*filepath", http.Dir(posterPath))
+	router.NotFound = static
+
+
+	log.Println("Starting Server")
+    log.Fatal(http.ListenAndServe(":4173", router))
 }
 
-const MAX_UPLOAD_SIZE = 10 * 1024 * 1024 // 1MB
 
-func Index(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	enableCors(&w)
 	switch r.Method {
 		case "OPTIONS" : {
@@ -100,7 +123,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 			}
 			//~ newFileName := fileName + fileEndings
 			//~ newPath := filepath.Join(uploadPath, newFileName)
-			newFileName := fmt.Sprintf("%d%s", time.Now().UnixNano(), fileEndings)
+			//~ newFileName := fmt.Sprintf("%d%s", time.Now().UnixNano(), fileEndings)
+			newFileName := fmt.Sprintf("%d%s", GenerateFileName(16), fileEndings)
 			fmt.Println(newFileName)
 			newPath := filepath.Join(folderpath, newFileName)
 			//~ newPath := fmt.Sprintf("./images/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename))
@@ -119,7 +143,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 				renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
 			}
 			fmt.Println(r.Body)
-		//	fmt.Fprintf(w, "done")
+			fmt.Fprintf(w, newFileName)
 		}
 		default: {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -129,6 +153,57 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func GenerateFileName(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyz0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rndm.Intn(len(letters))]
+    }
+    return string(b)
+}
+
+func init() {
+    rndm.Seed(time.Now().UnixNano())
+}
+
+
+//~ func sendImageAsHTML(w http.ResponseWriter, r *http.Request, a string) {
+	//~ fmt.Fprintf(w,a)
+//~ }
+
+//~ func sendImageAsAttachment(w http.ResponseWriter, r *http.Request) {
+    //~ buf, err := os.ReadFile("F46ZPQ0bQAACFYs.jpg")
+    //~ if err != nil {
+        //~ log.Fatal(err)
+    //~ }
+    //~ w.Header().Set("Content-Type", "image/jpeg")
+    //~ w.Header().Set("Content-Disposition", `attachment;filename="F46ZPQ0bQAACFYs.jpg"`)
+    //~ w.Write(buf)
+//~ }
+
+//~ func sendImageAsBytes(w http.ResponseWriter, r *http.Request, a string) {
+    //~ buf, err := os.ReadFile("./uploads/"+a)
+    //~ if err != nil {
+        //~ log.Fatal(err)
+    //~ }
+    //~ w.Header().Set("Content-Type", "image/jpeg")
+    //~ w.Write(buf)
+//~ }
+
+//~ func DeleteEdit(filename string) {
+	//~ time.Sleep(10 * time.Second)  
+	//~ os.Remove(filename)
+	//~ fmt.Println(filename,"Deleted")
+//~ }
+
+
+func renderError(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(statusCode)
+	w.Write([]byte(message))
+}
+
+//curl -F "file=@D:\Garbage\Blackpink\JisooCheeks[youtube@ouSU8JvC4vg]-3.webm" localhost:4000/upload/
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
@@ -136,9 +211,26 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-//curl -F "file=@D:\Garbage\tmp\saerom.gif" localhost:4000/upload/ 
-func renderError(w http.ResponseWriter, message string, statusCode int) {
-	//~ enableCors(&w)
-	w.WriteHeader(statusCode)
-	w.Write([]byte(message))
+func rndmToken(len int) int64 {
+	b := make([]byte, len)
+	n, _ := rand.Read(b)
+	return int64(n)
 }
+
+//~ func XHRrespond(w http.ResponseWriter, message string) {
+	//~ fmt.Fprintf(w, message)
+//~ }
+
+//~ func EncrypIt(strToHash string) string {
+	//~ data := []byte(strToHash)
+	//~ return fmt.Sprintf("%x", md5.Sum(data))
+//~ }
+
+//~ func SessionVerify(sessionKey string) string {
+	//~ return fmt.Sprintf(sessionKey)
+//~ }
+
+func Ignore(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w, r, "favicon.png")
+}
+
